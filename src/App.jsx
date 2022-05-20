2192e163a08e0a7c4d7c2604b0c1809c5025e4c2
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-19 22:54:28 -0700

    adds components and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+
+import Header from "./components/Header";
+// import FeedbackItem from "./components/FeedbackItem";
+import {useState} from "react";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
 
 const App = () => {
-
+const [feedback, setFeedback] = useState(FeedbackData)
   return (
+    <>
+    <Header />
     <div className="app">
-       <h1>my app</h1>
+      <FeedbackList feedback={feedback}/>
     </div>
+    </>
   )
 };
+
+
 export default App;
